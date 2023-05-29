e40b4306308f10553c59254ecf46b41b787a4c64
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-05-30 00:55:22 +0600

    Component and Props

--- src/index.js
+++ src/index.js
@@ -1,55 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = document.querySelector("#root");
+const root = document.getElementById("root");
+//-------------------------->
+// function Clock() {
+//     return (
+//     <h1 className="heading">
+//       <span className="text">Hello {new Date().toLocaleTimeString()}</span>
+//     </h1>
+//   );
+// }
+// // ReactDOM.createRoot(root).render(Clock());
+// ReactDOM.createRoot(root).render(<Clock />);
 
-// const element = React.createElement('h1',null,'Hello World!');
+// giving attributes ------------>
 
-// const element = <h1>Hello World!</h1>
-// console.log(element)
+// function Clock({locale}) {
+//   return (
+//     <h1 className="heading">
+//       <span className="text">
+//         Hello {new Date().toLocaleTimeString(locale)}
+//       </span>
+//     </h1>
+//   );
+// }
+// ReactDOM.createRoot(root).render(<Clock locale="bn-BD"/>);
 
-//Part 1
-function greet(user) {
-    if (user) {
-      return `Hello ${user}`;
-    }
-    return "Hello stranger";
+// Using Class ---------------->
+
+// class Clock extends React.Component {
+//   render() {
+//     return (
+//       <h1 className="heading">
+//         <span className="text">Hello {new Date().toLocaleTimeString(this.props.locale)}</span>
+//       </h1>
+//     );
+//   }
+// }
+// ReactDOM.createRoot(root).render(<Clock locale="bn-BD"/>);
+
+
+
+
+// we can make children element using this ------------->ummmmah is the children element 
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <h1 className="heading">
+        <span className="text">Hello {this.props.children} {new Date().toLocaleTimeString(this.props.locale)}</span>
+      </h1>
+    );
   }
-  const element2 = <h1>{greet("Rafio")}</h1>;
-  // ReactDOM.createRoot(root).render(element2);
-
-//Part 2
-
-function greetings(user) {
-  if (user) {
-    return <b>Hello {user}</b>;
-  }
-  return <b>Hello stranger</b>;
 }
-const element3 = greetings("Zuheb");
-console.log(element3);
-// ReactDOM.createRoot(root).render(element3);
-
-// Part 3
-const index = 0;
-const element4 = (
-  <h1 className="heading" tabIndex={index}>
-    <span>Hello World!</span>
-    <img src="" alt=""></img>
-  </h1>
-);
-
-console.log(element4);
-// ReactDOM.createRoot(root).render(element4);
-
-//Part 4
-const index1 = 0;
-setInterval(() => {
-  const element5 = (
-    <h1 className="heading" tabIndex={index1}>
-      <span className="text">Hello {new Date().toLocaleTimeString()}</span>
-      <img src="" alt=""></img>
-    </h1>
-  );
-  ReactDOM.createRoot(root).render(element5)
-}, 1000);
+ReactDOM.createRoot(root).render(<Clock locale="bn-BD">ummmmmmmah</Clock>);
