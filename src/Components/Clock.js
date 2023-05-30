1a8cb952f0d25826bce43fb9738df4dff7c46faa
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-05-30 15:52:02 +0600

    Finishing

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,61 +1,62 @@
-// import React from "react";
-
 //-------------------------------> Using Class
+import React from "react";
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
 
-// class Clock extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state = {date:new Date()}
-//   }
+  componentDidMount() {
+    this.clocktimer = setInterval(() => this.tick, 1000);
+  }
 
-//   componentDidMount(){
-//     setInterval(()=>{
-//       this.setState({
-//         date:new Date(),
-//       })
-//     },1000)
-//   }
+  componentWillUnmount() {
+    clearInterval(this.clocktimer);
+  }
 
-//   render() {
-//     return (
-//       <h1 className="heading">
-//         <span className="text">
-//           {this.state.date.toLocaleTimeString(this.props.locale)}
-//         </span>
-//       </h1>
-//     );
-//   }
-// }
-// export default Clock;
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
 
-
+  render() {
+    return (
+      <h1 className="heading">
+        <span className="text">
+          {this.state.date.toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    );
+  }
+}
+export default Clock;
 
 // ---------------------------------->Using Function
 
+// import React, { useState, useEffect } from 'react';
 
-import React, { useState, useEffect } from 'react';
+// function Clock(props) {
+//   const [date, setDate] = useState(new Date());
 
-function Clock(props) {
-  const [date, setDate] = useState(new Date());
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setDate(new Date());
+//     }, 1000);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, []);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+//   return (
+//     <h1 className="heading">
+//       <span className="text">
+//         {date.toLocaleTimeString(props.locale)}
+//       </span>
+//     </h1>
+//   );
+// }
 
-  return (
-    <h1 className="heading">
-      <span className="text">
-        {date.toLocaleTimeString(props.locale)}
-      </span>
-    </h1>
-  );
-}
-
-export default Clock;
+// export default Clock;
