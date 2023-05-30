b82ba89f2b2a2218240147518592c5f444be3419
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-05-30 14:16:22 +0600

    Add function

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,14 +1,61 @@
-import React from "react";
+// import React from "react";
 
-class Clock extends React.Component {
-  render() {
-    return (
-      <h1 className="heading">
-        <span className="text">
-          {new Date().toLocaleTimeString(this.props.locale)}
-        </span>
-      </h1>
-    );
-  }
+//-------------------------------> Using Class
+
+
+// class Clock extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {date:new Date()}
+//   }
+
+//   componentDidMount(){
+//     setInterval(()=>{
+//       this.setState({
+//         date:new Date(),
+//       })
+//     },1000)
+//   }
+
+//   render() {
+//     return (
+//       <h1 className="heading">
+//         <span className="text">
+//           {this.state.date.toLocaleTimeString(this.props.locale)}
+//         </span>
+//       </h1>
+//     );
+//   }
+// }
+// export default Clock;
+
+
+
+// ---------------------------------->Using Function
+
+
+import React, { useState, useEffect } from 'react';
+
+function Clock(props) {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <h1 className="heading">
+      <span className="text">
+        {date.toLocaleTimeString(props.locale)}
+      </span>
+    </h1>
+  );
 }
+
 export default Clock;
