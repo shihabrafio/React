1d67fae09dd396a4f34ffb81beb61eb48a78d706
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-04 13:55:35 +0600

    Add css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { useState } from 'react';
-import MyComponent from './components/MyComponent';
+import './assets/css/global.css';
+import Logo from './Components/Logo';
 
 export default function App() {
-    const [show, setShow] = useState(true);
-
     return (
-        <div className="app">
-            <div>{show && <MyComponent />}</div>
-            <p>
-                <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
-                    {show ? 'Hide post' : 'Show post'}
-                </button>
-            </p>
+        <div className="App">
+            <h1>Learn with Sumit</h1>
+            <Logo />
         </div>
     );
 }
